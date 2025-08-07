46d75ed743ef011d5f00d650e1b7151c79ea68c0
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-08-07 10:58:47 +0530

    testbench

--- Day - 05(07-08-2025)/code for half adder using tnterface and DUT/testbench.v
+++ Day - 05(07-08-2025)/code for half adder using tnterface and DUT/testbench.v
@@ -1,22 +1,37 @@
-
 interface  inter;
-  logic [2:0]a;
-  logic [2:0]b;
-  logic [3:0]c;
+  logic [1:0]a;
+  logic [1:0]b;
+  logic [2:0]sum;
+  logic [2:0]carry;
 endinterface
+
  module tb;
    inter aif();
-   add dut(.a(aif.a),.b(aif.b),.c(aif.c));
+   hadd dut(.a(aif.a),.b(aif.b),.sum(aif.sum),.carry(aif.carry));
    initial begin
-     aif.a=5;
-     aif.b=7;
+     aif.a=0;
+     aif.b=0;
      #10;
-     aif.a=6;
-     aif.b=7;
+     aif.a=0;
+     aif.b=1;
+     #10;
+     aif.a=1;
+     aif.b=0;
+     #10;
+     aif.a=1;
+     aif.b=1;
    end
    initial begin
-     $monitor ("a=%0d;b=%0d;c=%0d",aif.a,aif.b,aif.c);
+     $monitor("a=%0b,b=%0b,sum=%0b,carry=%0b",aif.a,aif.b,aif.sum,aif.carry);
      #100;
      $finish;
    end
    endmodule
+
+/*
+OUTPUT:
+a=0,b=0,sum=0,carry=0
+a=0,b=1,sum=1,carry=0
+a=1,b=0,sum=1,carry=0
+a=1,b=1,sum=0,carry=1
+*/
