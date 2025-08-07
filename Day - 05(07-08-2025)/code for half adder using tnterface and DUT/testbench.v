daa46f1c3526601c4dbb0685c4bd9b7f5ca49a2d
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-08-07 10:57:18 +0530

    testbench

---
+++ Day - 05(07-08-2025)/code for half adder using tnterface and DUT/testbench.v
@@ -0,0 +1,22 @@
+
+interface  inter;
+  logic [2:0]a;
+  logic [2:0]b;
+  logic [3:0]c;
+endinterface
+ module tb;
+   inter aif();
+   add dut(.a(aif.a),.b(aif.b),.c(aif.c));
+   initial begin
+     aif.a=5;
+     aif.b=7;
+     #10;
+     aif.a=6;
+     aif.b=7;
+   end
+   initial begin
+     $monitor ("a=%0d;b=%0d;c=%0d",aif.a,aif.b,aif.c);
+     #100;
+     $finish;
+   end
+   endmodule
