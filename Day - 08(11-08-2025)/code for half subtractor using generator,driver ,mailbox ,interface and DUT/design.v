a704e859d03d2e343d23d651175f7188f8ad2a39
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-08-11 10:04:29 +0530

    design

---
+++ Day - 08(11-08-2025)/code for half subtractor using generator,driver ,mailbox ,interface and DUT/design.v
@@ -0,0 +1,18 @@
+//T.Guru Nandini Devi
+//nandinidevitekumudi@gmail.com
+module half_sub(
+  input  logic a,
+  input  logic b,
+  output logic sum,
+  output logic carry
+);
+  assign sum   = a ^ b;   // XOR for sum
+  assign carry = (~a) & b;   // AND for carry
+endmodule
+
+interface inter;
+  logic a;
+  logic b;
+  logic sum;
+  logic carry;
+endinterface
