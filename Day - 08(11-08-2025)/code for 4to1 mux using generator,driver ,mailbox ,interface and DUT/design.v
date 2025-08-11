2dff2ccc5f6deda0da4e80b5d2e2f76560acd121
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-08-11 11:54:36 +0530

    4to1mux (user defined)

---
+++ Day - 08(11-08-2025)/code for 4to1 mux using generator,driver ,mailbox ,interface and DUT/design.v
@@ -0,0 +1,15 @@
+//T.Guru Nandini Devi
+//23A91A0460
+module mux4to1 (
+  input  logic [3:0] d,
+  input  logic [1:0] sel,
+  output logic       y
+);
+  assign y = d[sel];
+endmodule
+
+interface mux4_if;
+    logic d;
+    logic sel;
+    logic y;
+endinterface
