08bc6f1b50744754af02cc38e40bd2a8ea03a7b7
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-08-14 11:29:37 +0530

    Full Subtractor

--- Day - 05/code for Full subtractor using generator,driver,mailbox,interface and DUT/tb.v
+++ Day - 05/code for Full subtractor using generator,driver,mailbox,interface and DUT/tb.v
@@ -30,14 +30,14 @@ class driver;
       vif.c=temp[0];
       
     #10;
-      $display("Driver applied test cases : a=%0b,b=%0b c=%0b and output from dut sum =%0b, carry=%0b",vif.a,vif.b,vif.c,vif.sum,vif.carry);
+      $display("Driver applied test cases : a=%0b,b=%0b c=%0b and output from dut diff =%0b, borrow=%0b",vif.a,vif.b,vif.c,vif.diff,vif.borrow);
     end
   endtask
 endclass
 
 module tb;
 
-  full_add u1(aif.a,aif.b,aif.c,aif.sum,aif.carry);
+  full_add u1(aif.a,aif.b,aif.c,aif.diff,aif.borrow);
   inter aif();
   generator gen;
   mailbox mbx;
@@ -65,14 +65,14 @@ OUTPUT:
 # generator test cases : a=1, b=0 c=1
 # generator test cases : a=1, b=1 c=0
 # generator test cases : a=1, b=1 c=1
-# Driver applied test cases : a=0,b=0 c=0 and output from dut sum =0, carry=0
-# Driver applied test cases : a=0,b=0 c=1 and output from dut sum =1, carry=1
-# Driver applied test cases : a=0,b=1 c=0 and output from dut sum =1, carry=1
-# Driver applied test cases : a=0,b=1 c=1 and output from dut sum =0, carry=1
-# Driver applied test cases : a=1,b=0 c=0 and output from dut sum =1, carry=0
-# Driver applied test cases : a=1,b=0 c=1 and output from dut sum =0, carry=0
-# Driver applied test cases : a=1,b=1 c=0 and output from dut sum =0, carry=0
-# Driver applied test cases : a=1,b=1 c=1 and output from dut sum =1, carry=1
+# Driver applied test cases : a=0,b=0 c=0 and output from dut diff =0, borrow=0
+# Driver applied test cases : a=0,b=0 c=1 and output from dut diff =1, borrow=1
+# Driver applied test cases : a=0,b=1 c=0 and output from dut diff =1, borrow=1
+# Driver applied test cases : a=0,b=1 c=1 and output from dut diff =0, borrow=1
+# Driver applied test cases : a=1,b=0 c=0 and output from dut diff =1, borrow=0
+# Driver applied test cases : a=1,b=0 c=1 and output from dut diff =0, borrow=0
+# Driver applied test cases : a=1,b=1 c=0 and output from dut diff =0, borrow=0
+# Driver applied test cases : a=1,b=1 c=1 and output from dut diff =1, borrow=1
 */
 
 
