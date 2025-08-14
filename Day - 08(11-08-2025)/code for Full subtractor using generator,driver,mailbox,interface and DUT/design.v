c75a88e49c526f3e3b21d05db506203e2f1dd0c6
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-08-14 11:14:53 +0530

    Full subtractor

--- Day - 08(11-08-2025)/code for Full subtractor using generator,driver,mailbox,interface and DUT/design.v
+++ Day - 08(11-08-2025)/code for Full subtractor using generator,driver,mailbox,interface and DUT/design.v
@@ -1,21 +1,21 @@
 //T.Guru Nandini Devi
 //23A91A0460
 
-module full_add(
+module full_sub(
   input  logic a,
   input  logic b,
   input  logic c,
   output logic sum,
   output logic carry
 );
-  assign sum   = a ^ b ^ c;   // XOR for sum
-  assign carry = (~a&b) | (b&c) | (c& ~a);   // AND for carry
+  assign diff   = a ^ b ^ c;   // XOR for difference
+  assign borrow = (~a&b) | (b&c) | (c& ~a);   // AND for borrow
 endmodule
 
 interface inter;
   logic a;
   logic b;
   logic c;
-  logic sum;
-  logic carry;
+  logic diff;
+  logic borrow;
 endinterface
